693a6dde31e3419c5bbe837fefdb32ee5a3a287d
Abhishek Gujar <gujar98abhi@gmail.com>
2023-01-24 16:18:48 +0530

    updates on contact us

--- src/features/frontend/about-us/AboutUs.tsx
+++ src/features/frontend/about-us/AboutUs.tsx
@@ -3,7 +3,11 @@ import * as React from "react";
 interface IAboutUsProps {}
 
 const AboutUs: React.FunctionComponent<IAboutUsProps> = (props) => {
-  return <></>;
+  return (
+    <>
+      <h2>About us</h2>
+    </>
+  );
 };
 
 export default AboutUs;
