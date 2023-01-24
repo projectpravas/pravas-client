690b8d7ce322c24fe87a604ae765501050897e52
sanjay <sanjaybelanekar07@gmail.com>
2023-01-24 16:02:21 +0530

    meging main to sanjay

--- src/features/frontend/contact-us/ContactUs.tsx
+++ src/features/frontend/contact-us/ContactUs.tsx
@@ -3,7 +3,11 @@ import * as React from "react";
 interface IContactUsProps {}
 
 const ContactUs: React.FunctionComponent<IContactUsProps> = (props) => {
-  return <></>;
+  return (
+    <>
+      <h2>Sanjay</h2>
+    </>
+  );
 };
 
 export default ContactUs;
