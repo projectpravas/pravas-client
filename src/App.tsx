cb85ebef0b14ea74aeb544f3b42b7a37c424433a
Yashwant Jamdade <jamdadeyashwant@gmail.com>
2023-01-25 17:45:30 +0530

    Static About us component completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import * as React from 'react';
+import AboutUs from './components/about-us/AboutUs';
+
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    {/* <h3>App </h3> */}
+    <AboutUs />
+  </div>;
 }
 
 export default App;
