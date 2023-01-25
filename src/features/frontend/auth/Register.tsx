8ec19b6a403d642fff8aef962821102768dbd674
sklearnearn <kshirsagar.shivam1919@gmail.com>
2023-01-25 15:21:42 +0530

    login page

---
+++ src/features/frontend/auth/Register.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+interface IRegisterProps {}
+
+const Register: React.FunctionComponent<IRegisterProps> = (props) => {
+  return (
+    <>
+      <h1>Register</h1>
+    </>
+  );
+};
+
+export default Register;
