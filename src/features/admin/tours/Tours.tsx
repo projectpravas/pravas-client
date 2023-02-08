c8ae6d2fdacf3d1c7c0e0f70cbc04c541b5048c1
sklearnearn <kshirsagar.shivam1919@gmail.com>
2023-02-08 23:42:04 +0530

    custom tour form validation

--- src/features/admin/tours/Tours.tsx
+++ src/features/admin/tours/Tours.tsx
@@ -8,8 +8,8 @@ const Tours: React.FunctionComponent<IToursProps> = (props) => {
   return (
     <>
       {/* <h1>Tours</h1> */}
-      <PackageForm />
-      {/* <CustomTourForm /> */}
+      {/* <PackageForm /> */}
+      <CustomTourForm />
     </>
   );
 };
